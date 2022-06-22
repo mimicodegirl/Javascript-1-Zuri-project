08d66e2eb7fedc154401da4096177b210a99c476
Mariam Banire <mariambanire@gmail.com>
2022-06-22 21:45:06 +0100

    first javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+const firstName = 'Banire Mariam .0.';
+const country = 'Nigeria';
+let height =  '175.26cm';
+
+alert('Name:' + ' ' + firstName + '\n'+ 'Country:' + ' ' + country + '\n' + 'Height:' + ' ' + height);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
